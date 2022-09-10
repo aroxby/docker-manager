7be401b72909b095fdc79f6b98bbaf33dd578fe1
Andy Roxby <aroxby@users.noreply.github.com>
2022-09-10 05:53:45 -0400

    Repeatedly try to shutdown the VM

--- orchestration/stop.sh
+++ orchestration/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-# FIXME: alpine doesn't respond to the shutdown signal?
+# FIXME: Current distro doesn't run acpid
 
 MACHINE_NAME=docker-manager-1
 SHUTDOWN_WAIT=10
@@ -9,17 +9,16 @@ vm_running() {
     VBoxManage list runningvms | grep $MACHINE_NAME > /dev/null
 }
 
-VBoxManage controlvm $MACHINE_NAME acpipowerbutton
-
 echo Waiting for shutdown...
 
 WAITED=0
 while vm_running && [ $WAITED -lt $SHUTDOWN_WAIT ]; do
+    VBoxManage controlvm $MACHINE_NAME acpipowerbutton
     WAITED=$(( $WAITED + 1 ))
     sleep 1
 done
 
 if [ $WAITED -ge $SHUTDOWN_WAIT ]; then
-    echo Forcing poweroff
+    echo Forcing poweroff...
     VBoxManage controlvm $MACHINE_NAME poweroff
 fi
