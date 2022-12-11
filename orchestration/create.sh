aroxby/docker-manager

commit 07bf7a870ac5bdeddb870b4d809e3521cb32a4bd

Use a dash compatible `SCRIPT_DIR` expression

orchestration/create.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" 1>/dev/null 2>&1 /dev/null && pwd )
 
 MACHINE_NAME=docker-manager-1
 HOST_TO_GUEST_REDUCTION=4

orchestration/wait-for-ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" 1>/dev/null 2>&1 /dev/null && pwd )
 
 ssh_up() {
     curl -m3 -s http://$1:22

orchestration/wait-for-start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" 1>/dev/null 2>&1 /dev/null && pwd )
 MACHINE_NAME=docker-manager-1
 
 box_up() {

commit afc5a579012f3d7d83402b0ff953370bfcebb9c5

Fix broken change to `SCRIPT_DIR` expression
This was broken in 07bf7a870ac5bdeddb870b4d809e3521cb32a4bd

orchestration/create.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" 1>/dev/null 2>&1 /dev/null && pwd )
+SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" 1>/dev/null 2>&1 && pwd )
 
 MACHINE_NAME=docker-manager-1
 HOST_TO_GUEST_REDUCTION=4

orchestration/wait-for-ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" 1>/dev/null 2>&1 /dev/null && pwd )
+SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" 1>/dev/null 2>&1 && pwd )
 
 ssh_up() {
     curl -m3 -s http://$1:22

orchestration/wait-for-start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" 1>/dev/null 2>&1 /dev/null && pwd )
+SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" 1>/dev/null 2>&1 && pwd )
 MACHINE_NAME=docker-manager-1
 
 box_up() {